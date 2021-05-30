a3592db8861a944d9df4d92be48590cbea36578d
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-29 21:31:09 -0300

    initial commit

---
+++ src/main/java/com/xfef0/thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.xfef0.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xfef0/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfef0.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

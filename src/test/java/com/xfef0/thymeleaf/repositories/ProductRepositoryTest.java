429f8eb27abfe13a0ab6ae54ad479f96e88f1750
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-08-28 12:53:09 -0300

    add integration test

---
+++ src/test/java/com/xfef0/thymeleaf/repositories/ProductRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.xfef0.thymeleaf.repositories;
+
+import com.xfef0.thymeleaf.ThymeleafApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ThymeleafApplication.class)
+@WebAppConfiguration
+public class ProductRepositoryTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}

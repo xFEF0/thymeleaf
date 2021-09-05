703d2d2a81a16e1c47511f88f3b5191a9c98acce
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-09-05 20:33:38 -0300

    add security configurations

--- src/main/java/com/xfef0/thymeleaf/configuration/SecurityConfiguration.java
+++ src/main/java/com/xfef0/thymeleaf/configuration/SecurityConfiguration.java
@@ -1,17 +1,34 @@
 package com.xfef0.thymeleaf.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll()
-        .and().authorizeRequests().antMatchers("/console/**").permitAll();
+        httpSecurity
+                .authorizeRequests().antMatchers("/", "/products", "/product/show/*", "/console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("admin").password("admin").roles("ADMIN")
+                .and().withUser("user").password("user").roles("USER");
     }
 }
